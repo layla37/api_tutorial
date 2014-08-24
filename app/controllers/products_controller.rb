be25db4bfe3ee14531ccbb361137b92c24ea1f5a
Layla <laylamandella@gmail.com>
2014-08-23 23:47:12 -0700

    Add Product model, controller, and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
-  def create
-  end
+ 
+ 	def new
+		@product = Product.new
+	end
 
-  def list
-  end
+	def create
+		@product = Product.new(product_params)
+		@product.save
+		@products = Product.all	
+	end
+private
+	def product_params
+		params.require(:product).permit(:name, :description, :price, :expires_on)
+	end
 end
